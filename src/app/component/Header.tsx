0ee9174cb1fa597c7200cfbe1f4fc9c9a4be8586
sujitshahi <shahisujit9@gmail.com>
2025-11-19 20:38:19 +0545

    changed

--- src/app/component/Header.tsx
+++ src/app/component/Header.tsx
@@ -17,7 +17,7 @@ import { Button } from "@/components/ui/button"
 
 const components: { title: string; href: string; description: string }[] = [
   {
-    title: "Alert Dialog",
+    title: "Fashion",
     href: "/docs/primitives/alert-dialog",
     description:
       "A modal dialog that interrupts the user with important content and expects a response.",
@@ -74,34 +74,7 @@ export function Header() {
         <NavigationMenuItem>
         
           <Button className="text-xl">Home</Button>
-          <NavigationMenuContent>
-            <ul className="grid gap-2 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="from-muted/50 to-muted flex h-full w-full flex-col justify-end rounded-md bg-linear-to-b p-4 no-underline outline-hidden transition-all duration-200 select-none focus:shadow-md md:p-6"
-                    href="/"
-                  >
-                    <div className="mb-2 text-lg font-medium sm:mt-4">
-                      shadcn/ui
-                    </div>
-                    <p className="text-muted-foreground text-sm leading-tight">
-                      Beautifully designed components built with Tailwind CSS.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
+          
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuTrigger className="text-xl">Men</NavigationMenuTrigger>
@@ -125,14 +98,17 @@ export function Header() {
           <NavigationMenuContent>
             <ul className="grid w-[300px] gap-4">
               <li>
-                <NavigationMenuLink asChild>
-                  <Link href="#">
-                    <div className="font-medium">Components</div>
-                    <div className="text-muted-foreground">
-                      Browse all components in the library.
-                    </div>
-                  </Link>
-                </NavigationMenuLink>
+              
+
+                {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
                 <NavigationMenuLink asChild>
                   <Link href="#">
                     <div className="font-medium">Documentation</div>
