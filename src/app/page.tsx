3b32966e57e0043ebd373cf31bfbc37bebaa1c44
sujitshahi <shahisujit9@gmail.com>
2025-11-19 13:03:45 +0545

    changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Header } from "./components/Header";
-import HomePage from "./components/HomePage";
+import { Header } from "./component/Header";
+import HomePage from "./component/HomePage";
 
 export default function Page() {
   return (
