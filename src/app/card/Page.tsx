79c918506d72f371930d5fd36d2cc6dd42c62369
sujitshahi <shahisujit9@gmail.com>
2025-11-21 10:27:11 +0545

    changed

--- src/app/card/Page.tsx
+++ src/app/card/Page.tsx
@@ -1,23 +1,82 @@
 import {Card, CardHeader, CardBody, Image} from "@heroui/react";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
   return (
-    <Card className="py-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4"
+      onClick={() => router.push('/productdetail')}>
+      <Card className="py-4">
+        <Image
+          alt="Card background"
+          className="object-cover rounded-xl"
+          src="https://images.unsplash.com/photo-1595341888016-a392ef81b7de?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHNob2VzfGVufDB8fDB8fHww"
+          width={270}
+        />
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <p className="text-tiny uppercase font-bold">Daily Mix</p>
+        <p className="text-tiny uppercase font-bold">Shoes</p>
         <small className="text-default-500">12 Tracks</small>
         <h4 className="font-bold text-large">Frontend Radio</h4>
       </CardHeader>
       <CardBody className="overflow-visible py-2">
-        <Image
-          alt="Card background"
-          className="object-cover rounded-xl"
-          src="https://heroui.com/images/hero-card-complete.jpeg"
-          width={270}
-        />
+        
       </CardBody>
     </Card>
+
+    <Card className="py-4">
+      <Image
+          alt="Card background"
+          className="object-cover rounded-xl"
+          src="https://images.unsplash.com/photo-1609587312208-cea54be969e7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2F0Y2hlc3xlbnwwfHwwfHx8MA%3D%3D"
+          width={270}
+        />
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+        <p className="text-tiny uppercase font-bold">Watches</p>
+        <small className="text-default-500">12 Tracks</small>
+        <h4 className="font-bold text-large">Frontend Radio</h4>
+      </CardHeader>
+      <CardBody className="overflow-visible py-2">
+        
+      </CardBody>
+    </Card>
+
+    <Card className="py-4">
+      <Image
+          alt="Card background"
+          className="object-cover rounded-xl"
+          src="https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHNoaXJ0c3xlbnwwfHwwfHx8MA%3D%3D"
+          width={270}
+        />
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+        <p className="text-tiny uppercase font-bold">jeans</p>
+        <small className="text-default-500">12 Tracks</small>
+        <h4 className="font-bold text-large">Frontend Radio</h4>
+      </CardHeader>
+      <CardBody className="overflow-visible py-2">
+        
+      </CardBody>
+    </Card>
+
+    <Card className="py-4">
+      <Image
+          alt="Card background"
+          className="object-cover rounded-xl"
+          src="https://plus.unsplash.com/premium_photo-1725075088969-73798c9b422c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c2hpcnRzfGVufDB8fDB8fHww"
+          width={270}
+        />
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+        <p className="text-tiny uppercase font-bold">shirts</p>
+        <small className="text-default-500">12 Tracks</small>
+        <h4 className="font-bold text-large">Frontend Radio</h4>
+      </CardHeader>
+      <CardBody className="overflow-visible py-2">
+        
+      </CardBody>
+    </Card>
+    </div>
     
 
   );
 }
+
+
